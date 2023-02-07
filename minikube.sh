55cced035407839c1b8a0f5e15b919542f9128e1
Yogendaroduru <123927531+Yogendaroduru@users.noreply.github.com>
2023-02-07 16:18:58 +0530

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+sudo apt-get update -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+minikube config set cpus 5
+minikube config set memory 17999
+
+minikube start
+
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
